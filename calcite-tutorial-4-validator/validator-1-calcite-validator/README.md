dabae17c87d82ad9d474a461bae546e82ba64d1d
quxiucheng <quxiucheng>
2019-05-10 20:07:50 +0800

    schema and model

--- calcite-tutorial-4-validator/validator-1-calcite-validator/README.md
+++ calcite-tutorial-4-validator/validator-1-calcite-validator/README.md
@@ -2,15 +2,44 @@
 
 ## 常见类
 
+### SqlValidatorScope
+```sql
+SELECT expr1
+FROM t1,
+    t2,
+    (SELECT expr2 FROM t3) AS q3
+WHERE c1 IN (SELECT expr3 FROM t4)
+ORDER BY expr4
+```
+
+```
+expr1 can see t1, t2, q3
+expr2 can see t3
+expr3 can see t4, t1, t2
+expr4 can see t1, t2, q3 加上（取决于方言）SELECT子句中定义的任何别名
+```
 ### SqlValidatorNamespace
 校验命名空间 
-
+```sql
+SELECT expr1
+FROM t1,
+    t2,
+    (SELECT expr2 FROM t3) AS q3
+WHERE c1 IN (SELECT expr3 FROM t4)
+ORDER BY expr4
+```
+```
+t1
+t2
+(SELECT expr2 FROM t3) AS q3
+(SELECT expr3 FROM t4)
+```
 ### SqlValidator
 
 ### SqlValidatorWithHints
 
 #### SqlValidatorImpl
 
-### SqlValidatorScope
+
 
 ### SqlValidatorTable
--- calcite-tutorial-4-validator/validator-1-calcite-validator/src/main/java/com/github/quxiucheng/calcite/validator/tutorial/Main1.java
+++ calcite-tutorial-4-validator/validator-1-calcite-validator/src/main/java/com/github/quxiucheng/calcite/validator/tutorial/Main1.java
@@ -2,48 +2,75 @@ package com.github.quxiucheng.calcite.validator.tutorial;
 
 import org.apache.calcite.config.CalciteConnectionConfigImpl;
 import org.apache.calcite.config.Lex;
-import org.apache.calcite.jdbc.CalciteSchema;
+import org.apache.calcite.jdbc.CalcitePrepare;
 import org.apache.calcite.prepare.CalciteCatalogReader;
 import org.apache.calcite.rel.type.RelDataTypeSystem;
+import org.apache.calcite.server.CalciteServerStatement;
 import org.apache.calcite.sql.SqlNode;
+import org.apache.calcite.sql.SqlSelect;
 import org.apache.calcite.sql.dialect.OracleSqlDialect;
 import org.apache.calcite.sql.fun.SqlStdOperatorTable;
 import org.apache.calcite.sql.parser.SqlParseException;
 import org.apache.calcite.sql.parser.SqlParser;
 import org.apache.calcite.sql.type.SqlTypeFactoryImpl;
+import org.apache.calcite.sql.validate.SqlConformanceEnum;
 import org.apache.calcite.sql.validate.SqlValidator;
+import org.apache.calcite.sql.validate.SqlValidatorScope;
 import org.apache.calcite.sql.validate.SqlValidatorUtil;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Properties;
+
 /**
+ * SqlOrderBy -> SqlSelect
+ * SqlMerge -> SqlSelect
+ * SqlDelete -> SqlSelect
+ * SqlUpdate -> SqlSelect
+ *
  * @author quxiucheng
  * @date 2019-04-26 10:18:00
  */
 public class Main1 {
-    public static void main(String[] args) throws SqlParseException {
+    public static void main(String[] args) throws SqlParseException, SQLException {
+
+        Properties info = new Properties();
+        info.put("lex", "mysql");
+        String model = "calcite-tutorial-3-schema/src/main/resources/model.json";
+        info.put("model", model);
+        Connection connection = DriverManager.getConnection("jdbc:calcite:", info);
+        CalciteServerStatement statement = connection.createStatement().unwrap(CalciteServerStatement.class);
+        CalcitePrepare.Context prepareContext = statement.createPrepareContext();
         // 解析配置 - mysql设置
         SqlParser.Config mysqlConfig = SqlParser.configBuilder().setLex(Lex.MYSQL).build();
         // 创建解析器
         SqlParser parser = SqlParser.create("", mysqlConfig);
         // Sql语句
-        String sql = "select * from emps where id = 1";
+        String sql = "select * from tutorial.user_info where id = 1";
         // 解析sql
         SqlNode sqlNode = parser.parseQuery(sql);
         // 还原某个方言的SQL
         System.out.println(sqlNode.toSqlString(OracleSqlDialect.DEFAULT));
-
+        // CalciteSchema rootSchema = CalciteSchema.createRootSchema(false, false);
         //note: 二、sql validate（会先通过Catalog读取获取相应的metadata和namespace）
 //note: get metadata and namespace
-//         SqlTypeFactoryImpl factory = new SqlTypeFactoryImpl(RelDataTypeSystem.DEFAULT);
-//         CalciteCatalogReader calciteCatalogReader = new CalciteCatalogReader(
-//                 CalciteSchema.from(rootScheme),
-//                 CalciteSchema.from(rootScheme).path(null),
-//                 factory,
-//                 new CalciteConnectionConfigImpl(new Properties()));
-//
-// //note: 校验（包括对表名，字段名，函数名，字段类型的校验。）
-//         SqlValidator validator = SqlValidatorUtil.newValidator(SqlStdOperatorTable.instance(), calciteCatalogReader, factory,
-//                 conformance(frameworkConfig));
-//         SqlNode validateSqlNode = validator.validate(sqlNode);
+        SqlTypeFactoryImpl factory = new SqlTypeFactoryImpl(RelDataTypeSystem.DEFAULT);
+        CalciteCatalogReader calciteCatalogReader = new CalciteCatalogReader(
+                prepareContext.getRootSchema(),
+                prepareContext.getDefaultSchemaPath(),
+                factory,
+                new CalciteConnectionConfigImpl(new Properties()));
 
+// //note: 校验（包括对表名，字段名，函数名，字段类型的校验。）
+        SqlValidator validator = SqlValidatorUtil.newValidator(SqlStdOperatorTable.instance(),
+                calciteCatalogReader, factory, SqlConformanceEnum.DEFAULT
+        );
+        // SqlValidatorScope scope = new SelectScope();
+
+        // SqlNode validateSqlNode = validator.validate(sqlNode);
+        SqlValidatorScope selectScope = validator.getSelectScope((SqlSelect) sqlNode);
+        System.out.println(selectScope);
+        // System.out.println(validateSqlNode);
     }
 }
