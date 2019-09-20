7b9b580790cdb05b3833b29d7cffca4215826aba
quxiucheng <quxiucheng>
2019-09-20 20:35:56 +0800

    validate教程

--- calcite-tutorial-4-validator/validator-1-calcite-validator/src/main/java/com/github/quxiucheng/calcite/validator/tutorial/IdentifierExpansionSample.java
+++ calcite-tutorial-4-validator/validator-1-calcite-validator/src/main/java/com/github/quxiucheng/calcite/validator/tutorial/IdentifierExpansionSample.java
@@ -7,8 +7,7 @@ import org.apache.calcite.prepare.CalciteCatalogReader;
 import org.apache.calcite.rel.type.RelDataTypeSystem;
 import org.apache.calcite.server.CalciteServerStatement;
 import org.apache.calcite.sql.SqlNode;
-import org.apache.calcite.sql.dialect.OracleSqlDialect;
-import org.apache.calcite.sql.fun.SqlNullifFunction;
+import org.apache.calcite.sql.dialect.MysqlSqlDialect;
 import org.apache.calcite.sql.fun.SqlStdOperatorTable;
 import org.apache.calcite.sql.parser.SqlParseException;
 import org.apache.calcite.sql.parser.SqlParser;
@@ -19,14 +18,15 @@ import org.apache.calcite.sql.validate.SqlValidatorUtil;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.Properties;
 
 /**
  * @author quxiucheng
- * @date 2019-05-23 15:49:00
+ * @date 2019-06-14 09:55:00
  */
-public class CallRewriteSample {
-    public static void main(String[] args) throws Exception {
+public class IdentifierExpansionSample {
+    public static void main(String[] args) throws SQLException, SqlParseException {
         Properties info = new Properties();
         info.put("lex", "mysql");
         String model = "calcite-tutorial-3-schema/src/main/resources/model.json";
@@ -39,11 +39,11 @@ public class CallRewriteSample {
         // 创建解析器
         SqlParser parser = SqlParser.create("", mysqlConfig);
         // Sql语句
-        String sql = "select coalesce('carrot','apple'),SECOND(timestamp '2008-9-23 01:23:45'),{fn ABS(1)},NULLIF('','1') from tutorial.user_info";
+        String sql = "select id from tutorial.user_info";
         // 解析sql
         SqlNode sqlNode = parser.parseQuery(sql);
         // 还原某个方言的SQL
-        System.out.println(sqlNode.toSqlString(OracleSqlDialect.DEFAULT));
+        System.out.println(sqlNode.toSqlString(MysqlSqlDialect.DEFAULT));
         // sql validate（会先通过Catalog读取获取相应的metadata和namespace）
         SqlTypeFactoryImpl factory = new SqlTypeFactoryImpl(RelDataTypeSystem.DEFAULT);
         CalciteCatalogReader calciteCatalogReader = new CalciteCatalogReader(
@@ -54,18 +54,10 @@ public class CallRewriteSample {
 
         // 校验（包括对表名，字段名，函数名，字段类型的校验。）
         SqlStdOperatorTable instance = SqlStdOperatorTable.instance();
-        instance.register(new SqlNullifFunction());
         SqlValidator validator = SqlValidatorUtil.newValidator(instance,
                 calciteCatalogReader, factory, SqlConformanceEnum.DEFAULT
         );
-        // 校验后的SqlNode
-        // SqlCoalesceFunction
-        // SqlDatePartFunction
-        // SqlJdbcFunctionCall - SqlFunction - rewrite - call =SqlJdbcFunctionCall
-        // SqlNewOperator NEW UDT（1,2）。
-        // SqlNullifFunction
-        // SqlProcedureCallOperator SELECT f(x) FROM VALUES(0) to SELECT * FROM TABLE f(x)
-        validator.setCallRewrite(true);
+        validator.setIdentifierExpansion(true);
         SqlNode validateSqlNode = validator.validate(sqlNode);
         System.out.println(validateSqlNode);
     }
